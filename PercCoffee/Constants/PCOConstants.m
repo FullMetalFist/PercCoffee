08173f0e3d112b36a51735a6249b41b5924ec1c0
Michael Vilabrera <m.vilabrera@gmail.com>
2015-03-16 11:35:34 -0400

    added Constants.m

---
+++ PercCoffee/Constants/PCOConstants.m
@@ -0,0 +1,15 @@
+//
+//  PCOConstants.m
+//  PercCoffee
+//
+//  Created by Michael Vilabrera on 3/5/15.
+//  Copyright (c) 2015 Giving Tree. All rights reserved.
+//
+
+#import "PCOConstants.h"
+
+@implementation PCOConstants
+
+NSString * const PERCOLATE_API_KEY = @"WuVbkuUsCXHPx3hsQzus4SE";
+
+@end
